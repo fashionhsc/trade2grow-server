63a656b9703fd9f28231991ade1ef29c9410d82c
Nishant Rathore <rnishant609@gmail.com>
2025-08-25 15:28:51 +0530

    check

--- src/controllers/admin/upload.js
+++ src/controllers/admin/upload.js
@@ -8,11 +8,11 @@ exports.uploadImageFile = tryCatch(async (req, res, next) => {
 
     res.status(200).json({ url: req.file.path, public_id: req.file.filename });
 
-})
+});
 
 exports.uploadVideoFile = tryCatch(async (req, res, next) => {
     if (!req.file) return next(new ErrorClass(`No file uploaded`, 400))
 
     res.status(200).json({ url: req.file.path, public_id: req.file.filename });
 
-})
+});
